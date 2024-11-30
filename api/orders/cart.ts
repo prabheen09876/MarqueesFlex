c691fe2c5072d6a22d6e0547e263a42bf14c8d9c
Abhishek <prabheen09876@gmail.com>
2024-11-30 11:20:41 +0530

    dfggrthdf

--- api/orders/cart.ts
+++ api/orders/cart.ts
@@ -20,7 +20,7 @@ interface CartOrder {
 }
 
 export default async function handler(req: any, res: any) {
-  // Add CORS headers
+  
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
