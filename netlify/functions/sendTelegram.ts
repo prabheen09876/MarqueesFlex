91325c7ad69adf5c47bb94437fdce2e1a55253d2
Abhishek <prabheen09876@gmail.com>
2025-01-04 16:31:02 +0530

    hmmmmmmmmmmmmmmmmmmmm

--- netlify/functions/sendTelegram.ts
+++ netlify/functions/sendTelegram.ts
@@ -1,8 +1,8 @@
 import { Handler } from '@netlify/functions';
 import { CartItem } from '../../src/types';
 
-const TELEGRAM_BOT_TOKEN = process.env.VITE_TELEGRAM_BOT_TOKEN;
-const TELEGRAM_CHAT_ID = process.env.VITE_TELEGRAM_CHAT_ID;
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
 interface OrderData {
     orderType: 'cart' | 'custom';
